4aa5a44b0b9214ca830fab8b91c4148d32cc0ec7
smswehla <smswehla>
2008-06-13 15:05:12 +0000

    [ 1934753 ] indCIMXMLExport.c fails to compile on gcc 4.3

--- indCIMXMLExport.c
+++ indCIMXMLExport.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: indCIMXMLExport.c,v 1.10 2008/05/01 20:59:15 mchasal Exp $
+ * $Id: indCIMXMLExport.c,v 1.11 2008/06/13 15:05:12 smswehla Exp $
  *
  * © Copyright IBM Corp. 2005, 2007
  *
@@ -243,7 +243,7 @@ static char *getErrorMessage(CURLcode err)
 {
     char error[4096];
 #if LIBCURL_VERSION_NUM >= 0x071200
-    error = curl_easy_strerror(err);
+    //error = curl_easy_strerror(err);
     snprintf(error,4095,"CURL error: %d (%s)",err,curl_easy_strerror(err));
 #else
     if ((err>0 && err<8) || (err>=22 && err<=23)) 
