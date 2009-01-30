4c490f23beb88105387cd85211d424fc02ccef42
buccella <buccella>
2009-01-30 23:55:38 +0000

    [ 2550426 ] Concurrency issue with LocalConnect

--- providerMgr.c
+++ providerMgr.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: providerMgr.c,v 1.56 2008/11/22 01:52:35 buccella Exp $
+ * $Id: providerMgr.c,v 1.57 2009/01/30 23:55:38 buccella Exp $
  *
  * © Copyright IBM Corp. 2005, 2007
  *
@@ -907,7 +907,7 @@ static void setInuseSem(void *id)
 int getProviderContext(BinRequestContext * ctx, OperationHdr * ohdr)
 {
    unsigned long int l;
-   int rc, i, x;
+   int rc=0, i, x;
    char *buf;
    ProvAddr *as;
    ComSockets sockets;
@@ -935,8 +935,16 @@ int getProviderContext(BinRequestContext * ctx, OperationHdr * ohdr)
    
    _SFCB_TRACE(1,("--- Sending mgr request - to %d from %d", sfcbSockets.send,
                 sockets.send)); 
-   spSendReq(&sfcbSockets.send, &sockets.send, buf, l, localMode);   
+   rc = spSendReq(&sfcbSockets.send, &sockets.send, buf, l, localMode);   
    free(buf);
+
+   if (rc < 0) {
+      mlogf(M_ERROR,M_SHOW,"--- spSendReq/spSendMsg failed to send on %d (%d)\n", sfcbSockets.send, rc);
+      ctx->rc = rc;
+      close (sockets.send);
+      close (sockets.receive);
+      return rc;
+   }
 
    _SFCB_TRACE(1, ("--- Sending mgr request done"));
    
