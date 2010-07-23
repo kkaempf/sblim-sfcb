a5399a066652c148a84bf225644b77e946d7d647
Klaus Kämpf <kkaempf@suse.de>
2010-07-23 11:32:05 +0200

    fileRepository.c: show path if dir open fails

--- fileRepository.c
+++ fileRepository.c
@@ -573,7 +573,7 @@ int existingNameSpace(const char *ns)
 #endif
     
    if ((d=opendir(fn))==NULL) {
-      perror("opendir");
+      fprintf(stderr, "opendir: %s: %s\n", fn, strerror(errno));
       return 0;
    }   
    closedir(d);
