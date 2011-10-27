072484d4b0dc5c2218c6dc3d01b8cda8ebde6005
Michael Chase-Salerno <bratac@linux.vnet.ibm.com>
2011-10-27 00:55:12 -0400

    [ 3428806 ] type=null in authorizeFilter call

--- providerDrv.c
+++ providerDrv.c
@@ -2520,7 +2520,7 @@ activateFilter(BinRequestHdr * hdr, ProviderInfo * info, int requestor)
   CMPIContext    *ctx = native_new_CMPIContext(MEM_TRACKED, info);
   CMPIResult     *result = native_new_CMPIResult(0, 1, NULL);
   CMPIFlags       flgs = 0;
-  char           *type = NULL;
+  char           *type = (char *) req->type.data;
 
   ctx->ft->addEntry(ctx, CMPIInvocationFlags, (CMPIValue *) & flgs,
                     CMPI_uint32);
@@ -2541,7 +2541,6 @@ activateFilter(BinRequestHdr * hdr, ProviderInfo * info, int requestor)
   if (se == NULL) {
     char           *query = (char *) req->query.data;
     char           *lang = (char *) req->language.data;
-    type = (char *) req->type.data;
     char           *sns = (char *) req->sns.data;
 
     se = (NativeSelectExp *) NewCMPISelectExp(query, lang, sns, NULL,
