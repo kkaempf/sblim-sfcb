1023f4ec4b3049b2bf895c5bf88a410eeff23973
Dave Heller <hellerda@us.ibm.com>
2012-10-24 10:59:36 -0400

    [ 3579637 ] SFCB should check ftok return code

--- msgqueue.c
+++ msgqueue.c
@@ -123,10 +123,16 @@ semSetValue(int semid, int semnum, int value)
 int
 initSem(int provs)
 {
+  _SFCB_ENTER(TRACE_MSGQUEUE, "initSem");
+
   union semun     sun;
   int             i;
 
-  sfcbSemKey = ftok(SFCB_BINARY, 'S');
+  if ((sfcbSemKey=ftok(SFCB_BINARY,'S')) <=0) {
+     mlogf(M_ERROR,M_SHOW,"-#- Error creating semaphore key using path: %s (%s)\n",
+         SFCB_BINARY, strerror(errno));
+    _SFCB_ABORT();
+  }
 
   if ((sfcbSem = semget(sfcbSemKey, 1, 0600)) != -1)
     semctl(sfcbSem, 0, IPC_RMID, sun);
@@ -157,7 +163,7 @@ initSem(int provs)
     semctl(sfcbSem, PROV_INUSE(i), SETVAL, sun);
     semctl(sfcbSem, PROV_ALIVE(i), SETVAL, sun);
   }
-  return 0;
+  _SFCB_RETURN(0);
 }
 
 int
