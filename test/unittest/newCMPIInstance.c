81f5b790162d0fd6d40ff4f2781d9506418c59d2
Michael Chase-Salerno <bratac@linux.vnet.ibm.com>
2013-02-07 12:56:40 -0500

    Unittest fixes for trimws

--- test/unittest/newCMPIInstance.c
+++ test/unittest/newCMPIInstance.c
@@ -24,6 +24,7 @@ extern CMPIInstance *internal_new_CMPIInstance(int mode,
                                                const CMPIObjectPath * cop,
                                                CMPIStatus *rc,
                                                int override);
+int trimws;
 
 int
 main(void)
