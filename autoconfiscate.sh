b15ae230bf22a10f13f41721f08a830f1b46efbb
Michael Chase-Salerno <bratac@linux.vnet.ibm.com>
2013-02-25 12:39:59 -0500

    [Sfcb:#7] autoconfiscate.sh needs --force for all tools

--- autoconfiscate.sh
+++ autoconfiscate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-aclocal &&
-autoheader &&
-libtoolize && 
+aclocal --force &&
+autoheader --force &&
+libtoolize --force && 
 automake -af &&
-autoconf &&
+autoconf --force &&
 #if test -d test; then cd test && ./autoconfiscate.sh && cd ..; fi
 if test -d mofc; then cd mofc && ./autoconfiscate.sh; fi
 if test -d cmpi-devel; then cd cmpi-devel && ./autoconfiscate.sh; fi
