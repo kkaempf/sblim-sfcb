946f5d2c4117682ff281422c867fa1f3a8df233a
Dave Heller <hellerda@us.ibm.com>
2013-05-08 20:20:08 -0400

    [sfcb-tix:#46] Error parsing CDATA with no whitespace

--- cimXmlParser.c
+++ cimXmlParser.c
@@ -963,7 +963,7 @@ static int procCdata(YYSTYPE * lvalp, ParserControl * parm)
    XmlAttr attr[1];
    
    if (tagEquals(parm->xmb, "![CDATA[")) {
-      parm->xmb->cur[0] = '>';   	
+      *(--parm->xmb->cur) = '>';
       char *v;
       if (attrsOk(parm->xmb, elm, attr, "![CDATA[", ZTOK_CDATA)) {
       	 
