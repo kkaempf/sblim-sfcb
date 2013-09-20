369f536e58e5153cca68f0cf11bf02a055b6b30d
buccella <buccella@linux.vnet.ibm.com>
2013-09-20 15:54:02 -0400

    [sfcb-tix:#75] fileRepository: show which directory could not be opened

--- fileRepository.c
+++ fileRepository.c
@@ -737,7 +737,7 @@ existingNameSpace(const char *ns)
 #endif
 
   if ((d = opendir(fn)) == NULL) {
-    perror("opendir");
+    fprintf(stderr, "opendir: %s: %s\n", fn, strerror(errno));
     return 0;
   }
   closedir(d);
